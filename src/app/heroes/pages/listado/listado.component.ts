139020447dffc330743afa6228748a94f7ade901
Oscar Davila <oscar7822002@gmail.com>
2022-08-08 14:53:26 -0500

    Heroes Service - Traer información de los héroes

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
+
+    this.heroesService.getHeroes()
+      .subscribe( resp => console.log(resp) );
+
   }
 
 }
