e4fc2d898ebe11dc342930af691b9669e7fef6a4
Oscar Davila <oscar7822002@gmail.com>
2022-08-11 15:40:49 -0500

    Insertar en base de datos

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -24,4 +24,8 @@ export class HeroesService {
   getSugerencias(termino: string): Observable<Heroe []> {
     return this.http.get<Heroe[]>(`${this.apiUrl}/heroes?q=${termino}&_limit=6`);
   }
+
+  agregarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.post<Heroe>(`${this.apiUrl}/heroes`, heroe);
+  }
 }
